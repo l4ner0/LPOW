d8452efe96e746b9636317ab0fd0d414c4abb447
l4ner0 <diegourbina1284@gmail.com>
2018-08-06 19:52:20 -0500

    Se dio completa funcionalidad a ambos graficos estadisticos

--- 3-Programación/BackEnd/Villabook/web/Complementos/js/Graficos.js
+++ 3-Programación/BackEnd/Villabook/web/Complementos/js/Graficos.js
@@ -28,61 +28,100 @@ function cargarGraficasArea(ruta,controlador,op){
             op:op
         },
         success:function(response){
-            alert(response);
             var valores=$.parseJSON(response);
+          var data=[
+              ['2018/01',0,0],
+              ['2018/02',0,0],
+              ['2018/03',0,0],
+              ['2018/04',0,0],
+              ['2018/05',0,0],
+              ['2018/06',0,0],
+              ['2018/07',0,0],
+              ['2018/08',0,0],
+              ['2018/09',0,0],
+              ['2018/10',0,0],
+              ['2018/11',0,0],
+              ['2018/12',0,0]
+          ]
             
+            for(var i=0; i<valores[0].length; i++){
+                if(valores[0][i] === null){
+                    break;
+                }else{
+                   for(var j=0 ; j<11; j++){
+                       if(data[j][0] === valores[0][i].substr(0,7)){
+                           data[j][1] =  data[j][1]+1;
+                       }
+                   }
+                }
+            }
+            
+            
+            
+            var valores=$.parseJSON(response);
+            for(var i=0; i<valores[1].length; i++){
+                if(valores[1][i] === null){
+                    break;
+                }else{
+                   for(var j=0 ; j<11; j++){
+                       if(data[j][0] === valores[1][i].substr(0,7)){
+                           data[j][2] =  data[j][2]+1;
+                       }
+                   }
+                }
+            }
             var months = ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
 
             Morris.Area({
               element: 'area-example',
               data: [{
-                m: '2015/01/09',
-                a: 3,
-                b: 5
+                m:"2018-01",
+                a: data[0][2],
+                b: data[0][1]
                }, {
-                m: '2015/02/09',
-                a: 3,
-                b: 5
+                m: "2018-02",
+                a: data[1][2],
+                b: data[1][1]
               }, {
-                m: '2015/03/03',
-                a: 3,
-                b: 5
+                m: "2018-03",
+               a: data[2][2],
+                b: data[2][1]
               }, {
-                m: '2015-04',
-                a: 2,
-                b: 3
+                m:"2018-04",
+                a: data[3][2],
+                b: data[3][1]
               }, {
-                m: '2015-05',
-                a: 5,
-                b: 8
+                m: "2018-05",
+               a: data[4][2],
+                b: data[4][1]
               }, {
-                m: '2015-06',
-                a: 3,
-                b: 1
+                m: "2018-06",
+               a: data[5][2],
+                b: data[5][1]
               }, {
-                m: '2015-07',
-                a: 7,
-                b: 9
+                m: "2018-07",
+                a: data[6][2],
+                b: data[6][1]
               }, {
-                m: '2015-08',
-                a: 12,
-                b: 4
+                m: "2018-08",
+                a: data[7][2],
+                b: data[7][1]
               }, {
-                m: '2015-09',
-                a: 7,
-                b: 6
+                m: "2018-09",
+               a: data[8][2],
+                b: data[8][1]
               }, {
-                m: '2015-10',
-                a: 8,
-                b: 2
+                m: "2018-10",
+                a: data[9][2],
+                b: data[9][1]
               }, {
-                m: '2015-11',
-                a: 12,
-                b: 2
+                m:"2018-11",
+               a: data[10][2],
+                b: data[10][1]
               }, {
-                m: '2015-12',
-                a: 5,
-                b: 2
+                m: "2018-12",
+                a: data[11][2],
+                b: data[11][1]
               }, ],
               xkey: 'm',
               ykeys: ['a', 'b'],
--- 3-Programación/BackEnd/Villabook/web/Vistas/Villabook/about.jsp
+++ 3-Programación/BackEnd/Villabook/web/Vistas/Villabook/about.jsp
@@ -15,8 +15,8 @@
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
             <div class="page-heading">
-              <h1>About Me</h1>
-              <span class="subheading">This is what I do.</span>
+              <h1>Información</h1>
+              <span class="subheading">¿Quienes somos?</span>
             </div>
           </div>
         </div>
@@ -27,9 +27,14 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur voluptates odit, fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam ducimus consectetur?</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus, minus!</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur magnam, excepturi aliquid ex itaque esse est vero natus quae optio aperiam soluta voluptatibus corporis atque iste neque sit tempora!</p>
+          <p>
+              Somos un equipo de estudiantes de la Universidad Nacional Federico 
+              Villarreal pertenecientes a la carrera de Ingeniería de Sistemas.
+              Bajo la supervisión del Ingeniero Ivan Petrlik Azabache hemos desarrollado
+              un sistema cuya finalidad es la de agilizar los préstamos de libros  en la biblioteca de 
+              la Facultadad de Ingeniería Idustrial y de Sistemas.
+          </p>
+         
         </div>
       </div>
     </div>
